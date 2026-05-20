44fb7adfffa64af54935b93e6cf6c8b8d4f5579c
GiovannyPuentes <giovannyandrey1906@gmail.com>
2026-05-19 19:18:12 -0500

    Delete backend-turnos directory

--- backend-turnos/src/main/java/com/mitierrita/backend_turnos/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mitierrita.backend_turnos.repository;
-
-import com.mitierrita.backend_turnos.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
